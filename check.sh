0fc726b0e9446755d53311164bd4746360432cb0
DevopsRizwan <rizwan418360@gmail.com>
2019-07-25 01:21:26 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dir="./terraform_play_for_consul_ha"
-dir_ansible="ansible_play_for_consul_ha"
+dir_ansible="./ansible_play_for_consul_ha"
 cd ${dir}/
 terraform init
 terraform apply -auto-approve
