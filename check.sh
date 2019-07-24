ded264330ed336b3dffa0381e5f08248bc327a54
DevopsRizwan <rizwan418360@gmail.com>
2019-07-25 01:57:30 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 dir="./terraform_play_for_consul_ha"
 dir_ansible="./ansible_play_for_consul_ha"
+
 cd ${dir}/
 terraform init
 terraform apply -auto-approve
-cd ../${dir_ansible}
 
+cd ../${dir_ansible}
 ansible-playbook -vvv create-file.yml
 sleep 10
 ansible-playbook -vvv consul-configure.yml
-
